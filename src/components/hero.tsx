77bed282db4b33da81ab4eafff582e829b6fd5fb
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2024-07-30 11:14:30 -0700

    ss

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,54 +1,48 @@
-import React, { useState } from "react";
+import React from "react";
+import Image from "next/image";
+import { useTranslations } from "next-intl";
 
-const Modal = ({ isOpen, onClose, onSubmit }) => {
-	const [name, setName] = useState("");
-	const [phone, setPhone] = useState("");
-
-	const handleSubmit = (event:any) => {
-		event.preventDefault();
-		onSubmit({ name, phone });
-		onClose();
-	};
-
-	if (!isOpen) return null;
+const Hero = () => {
+	const t = useTranslations("NavBar");
 
 	return (
-		<div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-			<div className="bg-white p-8 rounded-lg">
-				<h2 className="text-2xl mb-4">Buyurtma berish</h2>
-				<form onSubmit={handleSubmit}>
-					<div className="mb-4">
-						<label className="block text-gray-700">Ism:</label>
-						<input
-							type="text"
-							className="w-full p-2 border border-gray-300 rounded"
-							value={name}
-							onChange={(e) => setName(e.target.value)}
-							required
-						/>
-					</div>
-					<div className="mb-4">
-						<label className="block text-gray-700">Telefon raqam:</label>
-						<input
-							type="tel"
-							className="w-full p-2 border border-gray-300 rounded"
-							value={phone}
-							onChange={(e) => setPhone(e.target.value)}
-							required
-						/>
-					</div>
-					<div className="flex justify-end">
-						<button type="button" className="mr-4 p-2 bg-gray-500 text-white rounded" onClick={onClose}>
-							Cancel
-						</button>
-						<button type="submit" className="p-2 bg-green-500 text-white rounded">
-							Submit
-						</button>
-					</div>
-				</form>
+		<div className="relative w-full h-[500px]">
+			{" "}
+			<div className="absolute left-[10%] top-[15%]">
+				<p className="text-[#004290] text-[63px] font-bold">
+					{t("dunyosizni")} <br /> {t("kutmoda")}
+				</p>
+				<button className="border-none rounded-lg p-3 text-2xl bg-green-500 text-white cursor-pointer flex items-center justify-center">
+					Buyurtma berish
+				</button>
 			</div>
+			{/* Adjust height as needed */}
+			{/* Background Image */}
+			<Image
+				src="/Travel_Concepts_6 copy 1.png"
+				alt="Traveller"
+				objectFit="cover"
+				width={700}
+				height={400}
+				className="absolute right-20  h-full"
+			/>
+			{/* Foreground Image */}
+			<Image
+				src="/Traveller 1.png"
+				alt="Travel Concepts"
+				width={400}
+				height={400}
+				className="absolute right-[15%] bottom-[40px] "
+			/>
+			<Image
+				src="/Frame 3.png"
+				alt="Travel Concepts"
+				width={400}
+				height={400}
+				className="absolute left-[15%] bottom-[40px] "
+			/>
 		</div>
 	);
 };
 
-export default Modal;
+export default Hero;
