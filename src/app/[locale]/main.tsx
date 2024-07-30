79a5798b14bdd20c590a05f0b7ba229411c9d4f6
asilbeksagdullayev <asilbek1188akeng@gmail.com>
2024-07-30 11:39:58 -0700

    sd

--- src/app/[locale]/main.tsx
+++ src/app/[locale]/main.tsx
@@ -2,8 +2,8 @@ import NavBar from "@/components/header";
 import React from "react";
 import Hero from "../../components/hero";
 import About from "@/components/about";
-import Graph from './../../components/graph';
-import  Footer  from '@/components/footer';
+import Graph from "./../../components/graph";
+import Footer from "@/components/footer";
 import Species from "@/components/species";
 
 const Main = () => {
@@ -13,8 +13,8 @@ const Main = () => {
 			<Hero />
 			<Species />
 			<About />
-			<Graph/>
-			<Footer/>
+			<Graph />
+			<Footer />
 		</div>
 	);
 };
